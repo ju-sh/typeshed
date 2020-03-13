396c0c17cfd708bace8578ccb8892b13622b6c04
Julin S <48789920+ju-sh@users.noreply.github.com>
2020-03-13 23:39:20 +0530

    add model stubs

--- third_party/2and3/chardet/__init__.pyi
+++ third_party/2and3/chardet/__init__.pyi
@@ -1,4 +1,19 @@
+import sys
 from typing import Any
+
 from .universaldetector import UniversalDetector as UniversalDetector
 
 def __getattr__(name: str) -> Any: ...  # incomplete
+
+if sys.version_info >= (3, 8):
+    from typing import TypedDict
+else:
+    from typing_extensions import TypedDict
+
+class _ModelType(TypedDict):
+    char_to_order_map: Tuple[int]
+    precedence_matrix: Tuple[int]
+    typical_positive_ratio: float
+    keep_english_letter: bool
+    charset_name: str
+    language: str
